a1f7a96d3c5481d75bbe87352ed7720825f6eac3
Rick <henriquels5@al.insper.edu.br>
2025-11-15 15:49:55 -0300

    fix logic

--- questionameto_MHA.py
+++ questionameto_MHA.py
@@ -9,23 +9,9 @@ class QuestionamentoMHA(Scene):
 
         self.play(FadeOut(VGroup(text, text2)))
         self.wait(1)
-
-        text = Text('É aí que introduzímos o').shift(UP * 0.5)
-        text2 = Text('Multi-Head Attention', weight=BOLD).next_to(text, DOWN)
-        self.play(Write(VGroup(text, text2)), run_time=1.5)
-        self.wait(2)
-
-        self.play(FadeOut(text))
-
-        title = Text('Multi-Head Attention').to_edge(UP)
-
-        self.play(Transform(text2, title))
-        self.wait(0.1)
-        self.remove(text2)
-        self.add(title)
         
-        text = Text('Veja esta frase:', font_size=32).shift(UP * 1.5)
-        text2 = Text('"A galinha não atravessou a').next_to(text, DOWN * 3)
+        text = Text('Veja esta frase:', font_size=32).to_edge(UP)
+        text2 = Text('"A galinha não atravessou a').shift(UP * 0.5)
         text3 = Text('rua porque ela estava cansada"').next_to(text2, DOWN)
 
         self.play(Write(text))
@@ -41,6 +27,37 @@ class QuestionamentoMHA(Scene):
         self.play(Indicate(text3[18:25], scale_factor=1.1))
         self.wait(2)
 
-        self.play(FadeOut(VGroup(title, text2, text3)))
+        self.play(FadeOut(VGroup(text2, text3)))
+        self.wait(1)
         
+        subtext = Text('Se considerarmos o modelo de atenção tradicional', font_size=40).shift(UP * 1)
+        subtext2 = Text('a atenção do nosso modelo ficaria dividida', font_size=40).next_to(subtext, DOWN)
+        subtext3 = Text('entre as palavras "cansada", "galinha"', font_size=40).next_to(subtext2, DOWN)
+        subtext4 = Text('e todas as outras palavras presentes na frase', font_size=40).next_to(subtext3, DOWN)
+
+        self.play(Write(VGroup(subtext, subtext2, subtext3, subtext4)), run_time=3)
+        self.wait(5)
+
+        self.play(FadeOut(VGroup(subtext, subtext2, subtext3, subtext4)))
         self.wait(2)
+
+        subtext = Text('É como se, por exemplo, nosso modelo não', font_size=40).shift(UP * 1)
+        subtext2 = Text('tivesse 100% de certeza de que "ela" realmente se', font_size=40).next_to(subtext, DOWN)
+        subtext3 = Text('refere às palavras "galinha" e "cansada"', font_size=40).next_to(subtext2, DOWN)
+
+        self.play(Write(VGroup(subtext, subtext2, subtext3)), run_time=3)
+        self.wait(5)
+
+        self.play(FadeOut(VGroup(subtext, subtext2, subtext3)))
+        self.wait(2)
+
+        text = Text('É aí que introduzímos o').shift(UP * 0.5)
+        text2 = Text('Multi-Head Attention', weight=BOLD).next_to(text, DOWN)
+        self.play(Write(VGroup(text, text2)), run_time=1.5)
+        self.wait(2)
+
+        title = Text('Multi-Head Attention').to_edge(UP)
+
+        self.play(FadeOut(text))
+        self.play(Transform(text2, title))
+        self.wait(1)
